954275fb8a97df28b69e0bb6ebe0616199f08f45
Anastasia Botsula <nactya.botsula@gmail.com>
2023-10-09 12:23:42 +0300

    feat: implement snake movement and change direction by keys

--- random-game/script.js
+++ random-game/script.js
@@ -6,6 +6,8 @@ const canvasHeight = canvas.height;
 const rectWidth = 20;
 const rectHeight = 20;
 
+let dir;
+
 // Draw a grid on the field
 const drawRect = () => {
     context.fillStyle = '#b9c681';
@@ -23,6 +25,19 @@ const drawRect = () => {
 }
 drawRect();
 
+// The direction of the snake by pressing the keys
+document.addEventListener('keydown', function(event) {
+    if(event.code === 'ArrowLeft' && dir !== 'right') {
+        dir = 'left';
+    } else if(event.code === 'ArrowUp' && dir !== 'down') {
+        dir = 'up';
+    } else if(event.code === 'ArrowRight' && dir !== 'left') {
+        dir = 'right';
+    } else if(event.code === 'ArrowDown' && dir !== 'up') {
+        dir = 'down';
+    }
+});
+
 // Draw snake
 let snake = [];
 snake[0] = {
@@ -30,13 +45,68 @@ snake[0] = {
     y: Math.floor((canvasHeight / rectHeight) / 2) * rectHeight
 };
 
+let leftEyeX = 3, 
+    leftEyeY = 3,
+    rightEyeX = 13,
+    rightEyeY = 3;
+
 function drawSnake() {
+    context.clearRect(0, 0, canvasWidth, canvasHeight);
+    drawRect();
     for (let i = 0; i < snake.length; i++) {
         context.fillStyle = '#8cb6c0';
         context.fillRect(snake[i].x, snake[i].y, rectWidth, rectHeight);
-        context.fillStyle = '#f3d495';
     }
-    context.fillRect(snake[0].x + 3, snake[0].y + 3, 4, 4);
-    context.fillRect(snake[0].x + 13, snake[0].y + 3, 4, 4);
+    context.fillStyle = '#f3d495';
+    context.fillRect(snake[0].x + leftEyeX, snake[0].y + leftEyeY, 4, 4);
+    context.fillRect(snake[0].x + rightEyeX, snake[0].y + rightEyeY, 4, 4);
+
+    snakeAfterChangeDirection();
 }
 drawSnake();
+
+// Draw a new snake head after changing the direction
+function snakeAfterChangeDirection() {
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    snake.pop();
+
+    if (dir === 'left') {
+        snakeX -= rectWidth;
+        leftEyeX = 3;
+        leftEyeY = 13;
+        rightEyeX = 3;
+        rightEyeY = 3;
+    }
+    if (dir === 'up') {
+        snakeY -= rectWidth;
+        leftEyeX = 3;
+        leftEyeY = 3;
+        rightEyeX = 13;
+        rightEyeY = 3;
+    } 
+    if (dir === 'right') {
+        snakeX += rectWidth;
+        leftEyeX = 13;
+        leftEyeY = 3;
+        rightEyeX = 13;
+        rightEyeY = 13;
+    } 
+    if (dir === 'down') {
+        snakeY += rectWidth;
+        leftEyeX = 13;
+        leftEyeY = 13;
+        rightEyeX = 3;
+        rightEyeY = 13;
+    } 
+
+    let newSnakeHead = {
+        x: snakeX,
+        y: snakeY
+    };
+
+    snake.unshift(newSnakeHead);
+}
+
+let gameInterval = setInterval(drawSnake, 100);
